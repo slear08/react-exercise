487989219c9939ff7f13d2394f3daa8ddbd16a2f
slear08 <88441444+slear08@users.noreply.github.com>
2022-11-04 21:45:21 +0800

    Counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useState} from "react";
 
 function App() {
+
+  const [count , setCount] = useState(0);
+
+
   return (
-    <div className="App">
-      <h1>THIS IS MY REACT JS EXERCISE</h1>
+    <div>
+      <div className="container">
+        <button onClick={()=>{
+          setCount(count+1);
+        }}>Increase</button>
+        <button onClick={()=>{
+          setCount(count-1)
+        }}>Decrease</button>
+        <button onClick={()=>{
+          setCount(0)
+        }}>Reset</button>
+        <h1>{count}</h1>
+      </div>
     </div>
   );
 }
