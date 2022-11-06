f64993da51c57fe6a910d9edb5415d3fd0cc044c
slear08 <88441444+slear08@users.noreply.github.com>
2022-11-06 15:55:05 +0800

    Fetching API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Axios  from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [excuse,setExcuse] = useState("");
+
+  const fetchData = (excuse) =>{
+    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}`).then((response)=>{
+      setExcuse(response.data[0].excuse);
+    });
+  }
+
   return (
     <div className="App">
-      <h1>THIS IS MY REACT JS EXERCISE</h1>
+        <h1>Generate an Exuse</h1>
+        <button onClick={()=>{ fetchData("party")} }>Party</button>
+        <button onClick={()=>{ fetchData("family")} }>Family</button>
+        <button onClick={()=>{ fetchData("office")} }>Office</button>
+
+        <div className="excuse-container">
+          <h1>{excuse}</h1>
+        </div>
     </div>
   );
 }
